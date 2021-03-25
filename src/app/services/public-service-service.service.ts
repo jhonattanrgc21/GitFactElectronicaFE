84a730ca4a75805115ace90fe3c30aecd2e712da
Grace Brenes <gbrenes@credix.com>
2021-03-25 13:53:51 -0600

    E.2021 Issue with sent email was fixed

--- src/app/services/public-service-service.service.ts
+++ src/app/services/public-service-service.service.ts
@@ -1,131 +1,204 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
-
-import { PublicServiceReceiptDetailModalComponent } from '../public-service/modal/public-service-receipt-detail-modal-component/public-service-receipt-detail-modal-component.component';
-import { MatConfirmComponent } from '../mat-confirm/mat-confirm.component';
+import { Observable, Subject } from "rxjs";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PublicServiceServiceService {
   url = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  private resendEmailSuccessSubject: Subject<boolean>;
+  private resendEmailErrorSubject: Subject<boolean>;
 
+  constructor(private http: HttpClient) {
+    this.resendEmailSuccessSubject = new Subject<boolean>();
+    this.resendEmailErrorSubject = new Subject<boolean>();
+  }
 
-  getPublicServiceReceiptsByParams(consecutiveNumber,accountNumber,identification, dateFrom, dateTo, amountFrom, amountTo, stateId, paymethodId,saleconditionId,carteraId,doctypeId) {
-    const method = '/getbillfilter';
+  public get resendEmailSuccess$(): Observable<boolean> {
+    return this.resendEmailSuccessSubject.asObservable();
+  }
+
+  public get resendEmailError$(): Observable<boolean> {
+    return this.resendEmailErrorSubject.asObservable();
+  }
+
+  getPublicServiceReceiptsByParams(
+    consecutiveNumber,
+    accountNumber,
+    identification,
+    dateFrom,
+    dateTo,
+    amountFrom,
+    amountTo,
+    stateId,
+    paymethodId,
+    saleconditionId,
+    carteraId,
+    doctypeId
+  ) {
+    const method = "/getbillfilter";
     return this.http.post(
       this.url + method,
       JSON.stringify({
-        'numact': accountNumber, 'identification': identification, 'date1': dateFrom, 'date2': dateTo,
-        'catera': carteraId, 'state': stateId, 'medpay': paymethodId, 'salecondition': saleconditionId, 'amount': amountFrom, 'amount2': amountTo, 'doctype': doctypeId,
-        'consecutiveNumber':consecutiveNumber
+        numact: accountNumber,
+        identification: identification,
+        date1: dateFrom,
+        date2: dateTo,
+        catera: carteraId,
+        state: stateId,
+        medpay: paymethodId,
+        salecondition: saleconditionId,
+        amount: amountFrom,
+        amount2: amountTo,
+        doctype: doctypeId,
+        consecutiveNumber: consecutiveNumber,
       }),
-      { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
+      {
+        headers: new HttpHeaders()
+          .set("Content-Type", "text/plain")
+          .set("Accept", "*/*"),
+      }
     );
   }
 
- 
   getReceiptDetail(receiptId: number) {
-    const method = '/publicservice/findpublicservicereceiptdetailstring';
+    const method = "/publicservice/findpublicservicereceiptdetailstring";
     return this.http.post(
       this.url + method,
-      JSON.stringify({'publicServiceReceiptId': receiptId}),
-      { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
+      JSON.stringify({ publicServiceReceiptId: receiptId }),
+      {
+        headers: new HttpHeaders()
+          .set("Content-Type", "text/plain")
+          .set("Accept", "*/*"),
+      }
     );
   }
 
-
-  sendBillPDF(consecutiveNumber) {
-    const method = '/getbillbyconsecutive';
-    return this.http.post(
-      this.url + method,
-      JSON.stringify({'consecutiveNumber': consecutiveNumber}),
-      { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-    );
+  public resendBillPDF(consecutiveNumber: string, emailUser: string): void {
+    this.http
+      .post(
+        `${this.url}/getbillbyconsecutive`,
+        JSON.stringify({ consecutiveNumber, emailUser }),
+        {
+          headers: new HttpHeaders()
+            .set("Content-Type", "text/plain")
+            .set("Accept", "*/*"),
+        }
+      )
+      .subscribe((response: any) => {
+        if (response) {
+          this.resendEmailSuccessSubject.next(true);
+        } else {
+          this.resendEmailErrorSubject.next(true);
+        }
+      });
   }
 
   getPublicServicesList() {
-    const method = '/publicservice/findpublicservicelist';
+    const method = "/publicservice/findpublicservicelist";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getStateBill() {
+    const method = "/getstatebill";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getSaleCondition() {
+    const method = "/getsalecondition";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getDocType() {
+    const method = "/getdoctype";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getCartera() {
+    const method = "/getcartera";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getPayMethod() {
+    const method = "/getpaymethod";
+    return this.http.post(this.url + method, JSON.stringify({}), {
+      headers: new HttpHeaders()
+        .set("Content-Type", "text/plain")
+        .set("Accept", "*/*"),
+    });
+  }
+
+  getBillFilter(
+    consecutiveNumber,
+    accountNumber,
+    identification,
+    dateFrom,
+    dateTo,
+    amountFrom,
+    amountTo,
+    stateId,
+    paymethodId,
+    saleconditionId,
+    carteraId,
+    doctypeId
+  ) {
+    const method = "/getbillfilter";
     return this.http.post(
       this.url + method,
-      JSON.stringify({}),
-      { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
+      JSON.stringify({
+        numact: accountNumber,
+        identification: identification,
+        date1: dateFrom,
+        date2: dateTo,
+        catera: carteraId,
+        state: stateId,
+        medpay: paymethodId,
+        salecondition: saleconditionId,
+        amount: amountFrom,
+        amount2: amountTo,
+        doctype: doctypeId,
+        consecutiveNumber: consecutiveNumber,
+      }),
+      {
+        headers: new HttpHeaders()
+          .set("Content-Type", "text/plain")
+          .set("Accept", "*/*"),
+      }
     );
   }
 
-getStateBill(){
-  const method = '/getstatebill';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({}),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-getSaleCondition(){
-  const method = '/getsalecondition';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({}),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-getDocType(){
-  const method = '/getdoctype';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({}),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-
-getCartera(){
-  const method = '/getcartera';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({}),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-getPayMethod(){
-  const method = '/getpaymethod';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({}),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-
-
-getBillFilter(consecutiveNumber,accountNumber,identification, dateFrom, dateTo, amountFrom, amountTo, stateId, paymethodId,saleconditionId,carteraId,doctypeId) {
-  const method = '/getbillfilter';
-  return this.http.post(
-    this.url + method,
-    JSON.stringify({
-      'numact': accountNumber, 'identification': identification, 'date1': dateFrom, 'date2': dateTo,
-      'catera': carteraId, 'state': stateId, 'medpay': paymethodId, 'salecondition': saleconditionId, 'amount': amountFrom, 'amount2': amountTo, 'doctype': doctypeId,
-      'consecutiveNumber': consecutiveNumber
-    }),
-    { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
-  );
-}
-
-getPDF(consecutiveNumber) {
-    const method = '/pdfBill';
+  getPDF(consecutiveNumber) {
+    const method = "/pdfBill";
     return this.http.post(
       this.url + method,
-      JSON.stringify({'consecutiveNumber': consecutiveNumber}),
-      { headers: new HttpHeaders().set('Content-Type', 'text/plain').set('Accept', '*/*') }
+      JSON.stringify({ consecutiveNumber: consecutiveNumber }),
+      {
+        headers: new HttpHeaders()
+          .set("Content-Type", "text/plain")
+          .set("Accept", "*/*"),
+      }
     );
   }
-
-
-
-
 }
