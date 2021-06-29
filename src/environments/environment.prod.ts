485ebf75f30274a90d791e4cbad3eb4078825585
kchacon <kchacon@credix.com>
2021-06-29 10:44:03 -0600

    E.2021 Ajuste URL producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://micredix.com/sib/api',
-  apiLoginUrl: 'https://micredix.org/canalesbe/api',
+  apiLoginUrl: 'https://micredix.com/canalesbe/api',
   defaultPmnId: 4,
   defaultNmtId: 3
 };
