8546251a429f43d9a37615572327f583d07f3de7
Jhonattan Garcia <jgarciac@intelix.biz>
2024-08-05 12:37:57 -0400

    Inicio del modulo de reportes

---
+++ src/app/bill-reports/bill-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-reports',
+  templateUrl: './bill-reports.component.html',
+  styleUrls: ['./bill-reports.component.css']
+})
+export class BillReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,6 +10,25 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
   }
+  public ngOnDestroy(): void {
+
+  }
+
 
 }
